58cccc561ba4f8b5ffc2bdc232d2c07f286ef1e9
Ruchit Modi <modi.ruchit6@gmail.com>
2020-02-22 14:10:31 -0500

    Updated query for q3a

--- HW1/q3a.sql
+++ HW1/q3a.sql
@@ -1,4 +1,4 @@
-with cte as (
+with monthly_pill_counts as (
     select CONCAT(YEAR(TRANSACTION_DATE), RIGHT(CONCAT('0', MONTH(TRANSACTION_DATE)), 2)) as monthly_date,
            SUM(DOSAGE_UNIT)                                                               as pill_counts
     from CSE532.DEA_NY
@@ -7,4 +7,4 @@ with cte as (
 select monthly_date,
        pill_counts,
        AVG(pill_counts) over (order by monthly_date rows between 1 preceding and 1 following) as smooth_pill_counts
-from cte;
+from monthly_pill_counts;
