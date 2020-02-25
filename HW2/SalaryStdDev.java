32df1877045f1577bfe854fa6054e4aa0bb06b71
Ruchit Modi <modi.ruchit6@gmail.com>
2020-02-25 12:54:05 -0500

    Updated code to add parameter databasename

--- HW2/SalaryStdDev.java
+++ HW2/SalaryStdDev.java
@@ -34,11 +34,14 @@ public class SalaryStdDev {
 
 	public static void main(String[] args) {
 		try {
-			String dbURL = "jdbc:db2://172.17.0.2:50000/sample";
-			if (args.length == 3) {
-				String tablename = args[0];
-				String user = args[1];
-				String password = args[2];
+			String dbURL = "jdbc:db2://172.17.0.2:50000/%s";
+			if (args.length == 4) {
+				String databasename = args[0];
+				String tablename = args[1];
+				String user = args[2];
+				String password = args[3];
+
+				dbURL = String.format(dbURL, databasename);
 
 				String sql = "select power(sum(power(SALARY - avg(SALARY) over (order by SALARY rows between unbounded preceding and unbounded following), 2)) over (order by SALARY rows between unbounded preceding and unbounded following) / count(SALARY) over (order by SALARY rows between unbounded preceding and unbounded following), 0.5) as SALARYSTDDEV from %s limit 1";
 				sql = String.format(sql, tablename);
@@ -46,7 +49,7 @@ public class SalaryStdDev {
 				salaryStdDev.setDBConnection(dbURL, user, password);
 				salaryStdDev.printQueryResult(sql);
 			} else {
-				throw new Exception("Usage: java SalaryStdDev tablename user password");
+				throw new Exception("Usage: java SalaryStdDev databasename tablename user password");
 			}
 
 		} catch (Exception e) {
