5d2c73c6a0f93d12222b34e3f3b70ca233db35dc
Ruchit Modi <modi.ruchit6@gmail.com>
2020-03-01 20:29:18 -0500

    Added code documentation and comments

--- HW2/stddev.sql
+++ HW2/stddev.sql
@@ -1,13 +1,18 @@
+-- Create custom data type to store salaries in an array --
 CREATE OR REPLACE TYPE DECARRAY AS DECIMAL(9,2) ARRAY[INTEGER]@
 
+-- Create stored procedure --
 CREATE OR REPLACE PROCEDURE salary_std_dev(OUT SALARYSTDDEV DOUBLE)
     LANGUAGE SQL
     BEGIN
+
+        -- Create variables --
         DECLARE i INTEGER;
         DECLARE n DOUBLE;
         DECLARE avg_salary DOUBLE;
         DECLARE salary_array DECARRAY;
 
+        -- Fetch all salaries into salary_array --
         SET i = 1;
         FOR v1 AS c1 CURSOR FOR
             SELECT salary FROM EMPLOYEE
@@ -16,6 +21,7 @@ CREATE OR REPLACE PROCEDURE salary_std_dev(OUT SALARYSTDDEV DOUBLE)
             SET i = i + 1;
         END FOR;
 
+        -- Compute average salary --
         SET avg_salary = 0;
         SET n = CARDINALITY(salary_array);
         SET i = 1;
@@ -25,6 +31,7 @@ CREATE OR REPLACE PROCEDURE salary_std_dev(OUT SALARYSTDDEV DOUBLE)
         END WHILE;
         SET avg_salary = avg_salary / n;
 
+        -- Compute standard deviation --
         SET SALARYSTDDEV = 0;
         SET i = 1;
         WHILE i <= n DO
@@ -35,4 +42,5 @@ CREATE OR REPLACE PROCEDURE salary_std_dev(OUT SALARYSTDDEV DOUBLE)
 
     END@
 
+-- Call the stored procedure --
 CALL salary_std_dev(?)@
