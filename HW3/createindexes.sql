e1eb19f2839f359a1e78a59e1b1566ddb5829795
Ruchit Modi <modi.ruchit6@gmail.com>
2020-03-22 17:23:51 -0400

    Added hw3 files

---
+++ HW3/createindexes.sql
@@ -0,0 +1,10 @@
+drop index cse532.facilityidx;
+drop index cse532.zipidx;
+
+create index cse532.facilityidx on cse532.facility(geolocation) extend using db2gse.spatial_index(0.85, 2, 5);
+
+create index cse532.zipidx on cse532.uszip(shape) extend using db2gse.spatial_index(0.85, 2, 5);
+
+runstats on table cse532.facility and indexes all;
+
+runstats on table cse532.uszip and indexes all;
