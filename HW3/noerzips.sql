8565e96e1454d96737423635ee9f4a3849f8e12b
Ruchit Modi <modi.ruchit6@gmail.com>
2020-03-30 20:11:07 -0400

    Updated query to consider zipcodes in NY only

--- HW3/noerzips.sql
+++ HW3/noerzips.sql
@@ -6,10 +6,11 @@ with ERZIPCODES as (
     where ATTRIBUTEVALUE = 'Emergency Department'
 ),
      NEIGHBOR_ERZIPCODES as (
-         select ZCTA5CE10 as NEIGHBOR_ERZIPCODE
-         from CSE532.USZIP
+         select substr(ZIPCODE, 1, 5) as NEIGHBOR_ERZIPCODE
+         from CSE532.FACILITY F
+                  inner join CSE532.USZIP on substr(ZIPCODE, 1, 5) = ZCTA5CE10
                   inner join ERZIPCODES on DB2GSE.ST_Intersects(SHAPE, ERZIPCODE_SHAPE) = 1
      )
-select distinct ZCTA5CE10 as NOERZIPCODE
-from CSE532.USZIP
-where ZCTA5CE10 not in (select NEIGHBOR_ERZIPCODE from NEIGHBOR_ERZIPCODES);
+select distinct substr(ZIPCODE, 1, 5) as NOERZIPCODE
+from CSE532.FACILITY F
+where substr(ZIPCODE, 1, 5) not in (select NEIGHBOR_ERZIPCODE from NEIGHBOR_ERZIPCODES);
