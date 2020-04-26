966ad9cf70a48164aed354a937a12f752ab7283f
Ruchit Modi <modi.ruchit6@gmail.com>
2020-04-26 15:35:25 -0400

    Change log:
    - Added code to sort output by key
    - Added comments

--- HW4/src/main/java/SparkCovid19_1.java
+++ HW4/src/main/java/SparkCovid19_1.java
@@ -18,38 +18,42 @@ public class SparkCovid19_1 {
             SparkConf conf = new SparkConf().setMaster("local").setAppName("SparkCovid19_1");
             JavaSparkContext sc = new JavaSparkContext(conf);
 
+            // Get the start and end date
             Date startDate, endDate;
             try {
                 startDate = simpleDateFormat.parse(args[1]);
                 endDate = simpleDateFormat.parse(args[2]);
-            } catch (ParseException e) {
+            } catch (ParseException e) { // Exit if dates are in invalid format
                 System.out.println("Invalid date format. Use yyyy-MM-dd.");
                 e.printStackTrace();
                 return;
             }
 
-            JavaRDD<String> textFile = sc.textFile(args[0]);
+            JavaRDD<String> textFile = sc.textFile(args[0]); // read input file
             JavaPairRDD<String, Integer> counts = textFile
-                    .mapToPair(value -> {
+                    .mapToPair(value -> { // map the input to key value pair (location, new_deaths)
                         String[] split = value.split(",");
                         Date date;
                         try {
                             date = simpleDateFormat.parse(split[0]);
                         } catch (ParseException | IndexOutOfBoundsException e) {
+                            // Ignore lines with invalid date including header
                             return null;
                         }
-                        if (split.length == 4
-                                && (date.before(endDate) || date.equals(endDate))
-                                && (date.after(startDate) || date.equals(startDate))) {
+                        if (split.length == 4 // if date within specified interval
+                                && (date.after(startDate) || date.equals(startDate))
+                                && (date.before(endDate) || date.equals(endDate))) {
                             String location = split[1];
                             int newDeaths = Integer.parseInt(split[3]);
                             return new Tuple2<>(location, newDeaths);
                         } else {
+                            // Ignore dates outside specified interval
                             return null;
                         }
                     })
-                    .filter(Objects::nonNull)
-                    .reduceByKey(Integer::sum);
+                    .filter(Objects::nonNull) // filter out null values
+                    .reduceByKey(Integer::sum) // sum the new_deaths for each location (key)
+                    .sortByKey(); // sort output by location
 
             counts.saveAsTextFile(args[3]);
         } else {
